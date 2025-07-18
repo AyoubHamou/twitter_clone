216012cda67db4e1accdabe6131e141d529aef46
AyoubHamou <ahamou@student.1337.ma>
2025-07-18 18:55:25 +0100

    added the user routes&controllers

---
+++ backend/src/controllers/userControllers.js
@@ -0,0 +1,92 @@
+import asyncHandler from "express-async-handler";
+import User from "../models/User.js";
+import { clerkClient, getAuth } from "@clerk/express";
+import Notification from "../models/Notification.js";
+
+export const getUserProfile = asyncHandler(async (req, res) => {
+  const { username } = req.params;
+  const user = await User.findOne({ username });
+  if (!user) return res.status(404).json({ message: "User not Found" });
+  res.status(200).json({ user });
+});
+
+export const updateProfile = asyncHandler(async (req, res) => {
+  const { userId } = getAuth(req);
+  const user = await User.findByIdAndUpdate({ clerkId: userId }, req.body, {
+    new: true,
+  });
+  if (!user) return res.status(404).json({ message: "User not Found" });
+  res.status(200).json({ user });
+});
+
+export const syncUser = asyncHandler(async (req, res) => {
+  const { userId } = getAuth(req);
+  const userAlreadyExist = await User.findOne({ clerkId: userId });
+  if (userAlreadyExist)
+    return res
+      .status(200)
+      .json({ user: userAlreadyExist, message: "User Already Exist" });
+  const clerkUser = await clerkClient.users.getUser(userId);
+  const userData = {
+    clerkId: userId,
+    email: clerkUser.emailAddresses[0].emailAddress,
+    firstName: clerkUser.firstName || "",
+    lastName: clerkUser.lastName || "",
+    username: clerkUser.emailAddresses[0].emailAddress.split("@")[0],
+    profilePic: clerkUser.imageUrl || "",
+  };
+  const user = await User.create(userData);
+  res.status(201).json({ user, message: "User created successfully" });
+});
+
+export const getCurrentUser = asyncHandler(async (req, res) => {
+  const { userId } = getAuth(req);
+  const user = await User.findById({ clerkId: userId });
+  if (!user) return res.status(404).json({ message: "User not Found" });
+  res.status(200).json({ user });
+});
+export const followUser = asyncHandler(async (req, res) => {
+  const { userId } = getAuth(req);
+  const { targetUserId } = req.params;
+
+  if (userId === targetUserId)
+    return res.status(400).json({ message: "You cannot follow yourself" });
+
+  const currentUser = await User.findOne({ clerkId: userId });
+  const targetUser = await User.findById(targetUserId);
+
+  if (!currentUser || !targetUser)
+    return res.status(404).json({ message: "User not found" });
+
+  const isFollowing = currentUser.following.includes(targetUserId);
+
+  if (isFollowing) {
+    // unfollow
+    await User.findByIdAndUpdate(currentUser._id, {
+      $pull: { following: targetUserId },
+    });
+    await User.findByIdAndUpdate(targetUserId, {
+      $pull: { followers: currentUser._id },
+    });
+  } else {
+    // follow
+    await User.findByIdAndUpdate(currentUser._id, {
+      $push: { following: targetUserId },
+    });
+    await User.findByIdAndUpdate(targetUserId, {
+      $push: { followers: currentUser._id },
+    });
+
+    await Notification.create({
+      from: currentUser._id,
+      to: targetUserId,
+      type: "follow",
+    });
+  }
+
+  res.status(200).json({
+    message: isFollowing
+      ? "User unfollowed successfully"
+      : "User followed successfully",
+  });
+});
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,14 +1,23 @@
 import express from "express";
+import cors from "cors"
+import {clerkMiddleware} from "@clerk/express"
+
 import ENV from "./config/env.js";
 import { connectDb } from "./config/db.js";
-
+import userRoutes from "./routes/userRoutes.js"
 const app = express();
+
+app.use(cors())
+app.use(express.json())
+app.use(clerkMiddleware());
 
 connectDb();
 
 app.get("/", (req, res) => {
   res.send("hello /");
 });
+
+app.use("/api/users", userRoutes)
 
 app.listen(ENV.PORT, () => {
   console.log(`listening on port ${ENV.PORT}`);
